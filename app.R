c991effbc71bce9a785522b2d0595e6efee44357
Stiven Barbosa <stiven.barbosa@grupotks.com>
2022-10-11 17:59:13 +0200

    Ok practice 2

--- app.R
+++ app.R
@@ -1,14 +1,17 @@
 library(bs4Dash)
 library(dplyr)
 library(DT)
+library(ggplot2)
+library(plotly)
+library(lubridate)
 
 sidebar <- bs4DashSidebar(
   status="orange",
   bs4SidebarUserPanel("Anderson Barbosa", image = "https://freesvg.org/img/comic-boy.png"),
   
   bs4SidebarMenu(
-    bs4SidebarMenuItem("Grafica", tabName = "graficaTab", icon = icon("signal")),
-    bs4SidebarMenuItem("Tabla", tabName = "tablaTab", icon = icon("th-list"), selected = TRUE)  
+    bs4SidebarMenuItem("Plot", tabName = "plotTab", icon = icon("chart-line"), selected = TRUE),
+    bs4SidebarMenuItem("Table", tabName = "tableTab", icon = icon("table-list"))  
   )
 )
 
@@ -18,33 +21,52 @@ body <- bs4DashBody(
     
     # Grafica Tab
     bs4TabItem(
-      tabName = "graficaTab",
-      fluidRow(
-        bs4Card(
-          width = 4,
-          title = "Filter",
-          status = "orange",
-          dateRangeInput("daterange1", "Date range:",
-                         start = "2001-01-01",
-                         end   = "2010-12-31"),
-          actionButton("do", "Click Me")
-        ),
-        bs4Card(
-          width = 8,
-          title = "Plot",
-          status = "orange",
-          sliderInput("slider2", "Number of observations:", 1, 100, 50)
-        )
-      )
-    ),
-    
-    # Tabla Tab
-    bs4TabItem(
-      tabName = "tablaTab",
+      tabName = "plotTab",
       fluidRow(
         column(
           width = 4, 
-          uiOutput("tabla_tab_filters")
+          
+          fluidRow(
+            column(
+              width = 12,
+              uiOutput("plot_tab_filters")
+            )
+          ),
+          
+          fluidRow(
+            
+            # BOX Range
+            bs4InfoBoxOutput("totalResults", width = 12),
+            bs4InfoBoxOutput("totalRange", width = 12)
+            
+          )
+          
+        ),
+        column(
+          width = 8,
+          bs4Card(
+            width = NULL,
+            title = "Plot (plotly)",
+            status = "orange",
+            
+            fluidRow(
+              column(
+                width = 12,
+                plotlyOutput("plot")  
+              )
+            )
+          )
+        ),
+      )
+    ),
+    
+    # Table Tab
+    bs4TabItem(
+      tabName = "tableTab",
+      fluidRow(
+        column(
+          width = 4, 
+          uiOutput("table_tab_filters")
         ),
         column(
           width = 8,
@@ -63,7 +85,7 @@ body <- bs4DashBody(
 
 
 ui <- bs4DashPage(
-  dashboardHeader("Practica 2"),
+  dashboardHeader("Practice 2"),
   sidebar = sidebar,
   body = body
 )
@@ -71,13 +93,97 @@ ui <- bs4DashPage(
 server <- function(input, output, session) {
   # https://alvaroferro.shinyapps.io/practica2/
   
-  values <- reactiveValues();
-  
   # Import's CSV
-  sunspots <- read.csv(file = 'data/sunspots.csv')
+  sunspots <- read.csv(file = 'data/sunspots.csv', colClasses = c('character', 'Date','numeric'))
   biostats <- read.csv(file = 'data/biostats.csv')[,c("Name", "Sex", "Age", "Height..in.", "Weight..lbs.")]
   
-  output$tabla_tab_filters <- renderUI({
+  values <- reactiveValues();
+  values$sunspots <- sunspots;
+  
+  #---------------------------------------------------------------
+  #------ Event's Plot Tabs
+  #---------------------------------------------------------------
+  
+  # Render Interface UI with params  
+  output$plot_tab_filters <- renderUI({
+    dateMin <- min(sunspots$Month);
+    dateMax <- max(sunspots$Month);
+    
+    bs4Card(
+      width = NULL,
+      title = "Filter",
+      status = "orange",
+      dateRangeInput(
+        "dateRangeMonth", 
+        "Date range:",
+        start = dateMin,
+        end   = dateMax,
+        min   = dateMin,
+        max   = dateMax
+      ),
+      actionButton("updatePlot", "Update", status = "success", icon = icon("refresh"))
+    )
+  })
+  
+  # Event click button update plot
+  observeEvent(input$updatePlot, {
+    dateMin <- input$dateRangeMonth[1];
+    dateMax <- input$dateRangeMonth[2];
+    data <- sunspots %>% dplyr::filter(Month >= dateMin, Month <= dateMax);
+    values$sunspots <- data;
+  })
+  
+  output$totalResults <- renderbs4InfoBox({
+    if(is.null(values$sunspots)) return(NULL)
+    
+    totalResults = nrow(values$sunspots);
+    
+    bs4InfoBox(
+      totalResults,
+      value = "Total Results",
+      icon = icon("th-list", lib = "glyphicon")
+    )
+  })
+  
+  output$totalRange <- renderbs4InfoBox({
+    if(is.null(values$sunspots)) return(NULL)
+    
+    dateMin <- min(values$sunspots$Month);
+    dateMax <- max(values$sunspots$Month);
+    totalRange <- paste(dateMin, " - ", dateMax);
+    
+    # Exact age
+    int <- interval(dateMin, dateMax)
+    years <- trunc(time_length(int, "year"), 0);
+    
+    # Exact month
+    year(dateMin) <- year(dateMax);
+    int <- interval(dateMin, dateMax)
+    months <- trunc(time_length(int, "month"), 0);
+    
+    bs4InfoBox(
+      totalRange,
+      value = "Min/Max Range Plot",
+      subtitle = paste("Years: ", years, "- Months: ", months),
+      icon = icon("calendar", lib = "glyphicon")
+    )
+  })
+  
+  # Render plot
+  output$plot <- renderPlotly({
+    if(is.null(values$sunspots)) return(NULL)
+    
+    plot <- ggplot(values$sunspots, aes(Month, Sunspots));
+    plot <- plot + geom_line()
+    ggplotly(plot)
+  })
+  
+  #---------------------------------------------------------------
+  #------ Event's Table Tabs
+  #---------------------------------------------------------------
+  
+  # Render Interface UI with params  
+  output$table_tab_filters <- renderUI({
     bs4Card(
       width = NULL,
       title = "Reactive Filter",
@@ -88,11 +194,9 @@ server <- function(input, output, session) {
     );
   })
   
-  
+  # Reactive Data Biostats
   getDataBiostats <- reactive({
-    if(is.null(input$age)){
-      return(NULL)
-    }
+    if(is.null(input$age)) return(NULL)
     
     data <- biostats %>% dplyr::filter(Age >= input$age, Height..in. >= input$height);
     if(input$sex != "ALL"){
@@ -101,11 +205,11 @@ server <- function(input, output, session) {
     return(data);
   })
   
+  # Event to render table Biostats
   output$tableBiostats <- renderDataTable({
     data <- getDataBiostats();
-    if(is.null(data)){
-      return(NULL);
-    }
+    if(is.null(data)) return(NULL);
+    
     
     DT::datatable(
       data, 
